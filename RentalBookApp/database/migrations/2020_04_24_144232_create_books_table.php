70ef274877b4e0796039211d4319fe5e0f8ff248
Shinya-Takiyoshi <garo.savior@gmail.com>
2020-04-25 00:20:49 +0900

    mygration機能試しup

---
+++ RentalBookApp/database/migrations/2020_04_24_144232_create_books_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->string('title');
+            $table->string('body');
+            $table->string('book_image_path');
+            $table->tinyInteger('rental_status')->unsigned()->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
