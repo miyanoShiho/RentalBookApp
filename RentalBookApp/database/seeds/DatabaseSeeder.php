677ad44fa8d510a9dea2911b46bc63174c693e36
Shinya-Takiyoshi <garo.savior@gmail.com>
2020-06-01 22:23:46 +0900

    お知らせテーブル作成

--- RentalBookApp/database/seeds/DatabaseSeeder.php
+++ RentalBookApp/database/seeds/DatabaseSeeder.php
@@ -13,7 +13,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             BooksTableSeeder::class,
-            CommentsTableSeeder::class
+            CommentsTableSeeder::class,
+            NoticesTableSeeder::class
         ]);
     }
 }
