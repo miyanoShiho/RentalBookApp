76adae8375aac1638b80d40a60685fced3796840
Shinya-Takiyoshi <garo.savior@gmail.com>
2020-07-19 12:59:31 +0900

    画面レイアウト修正

--- RentalBookApp/resources/views/bookdetail.blade.php
+++ RentalBookApp/resources/views/bookdetail.blade.php
@@ -7,38 +7,40 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-6">
             <div class="card">
                 <div class="card-header">{{ __($books->title) }}</div>
-                <div class="card-body w-100">
+                <div class="card-body">
                     <div class="row body-wrapper">
                         <img class="float-center" src="{{ __($books->book_image_path) }}" width="300" height="300">
                         <div class="rentalStatus w-100">
                             貸出状況：{{ __($books->rental_status) }}
                         </div>
-                        <textarea cols="80" rows="5" placeholder="説明">{{ __($books->body) }}</textarea>
+                        <textarea class="w-100" cols=" 80" rows="5" placeholder="説明">{{ __($books->body) }}</textarea>
                         <div class="col-md-4">
                             <b>ユーザー名：{{ __($books->user->name) }}</b>
                         </div>
                         <div class="col-md-4">
                         </div>
-                        <form action="/bookdetail/commentDelete" method="POST">
-                            @csrf
-                            <div class="comment-wrapper">
-                                @foreach($comments as $comment)
-                                <div>
-                                    <label>{{$comment->user->name}}</label>
+                        <div class="col-12">
+                            <form action="/bookdetail/commentDelete" method="POST">
+                                @csrf
+                                <div class="comment-wrapper">
+                                    @foreach($comments as $comment)
+                                    <div>
+                                        <label>{{$comment->user->name}}</label>
+                                    </div>
+                                    <div>
+                                        <label>{{$comment->body}}</label>
+                                    </div>
+                                    <input type="hidden" name="comment_id" value="{{$comment->comment_id}}">
+                                    <input type="hidden" name="book_id" value="{{$book_id}}">
+                                    <input type="submit" class="btn btn-secondary float-right" name="delete" onClick="delete_alert(event);return false;" value="削除">
+                                    <hr>
+                                    @endforeach
                                 </div>
-                                <div>
-                                    <label>{{$comment->body}}</label>
-                                </div>
-                                <input type="hidden" name="comment_id" value="{{$comment->comment_id}}">
-                                <input type="hidden" name="book_id" value="{{$book_id}}">
-                                <input type="submit" class="btn btn-secondary float-right" name="delete" onClick="delete_alert(event);return false;" value="削除">
-                                <hr>
-                                @endforeach
-                                <div>
-                        </form>
+                            </form>
+                        </div>
                         <form action="/bookdetail/commentSave" method="POST">
                             @csrf
                             <table>
@@ -47,7 +49,7 @@
                                 @endif
                                 <tr>
                                     <td>
-                                        <textarea cols="80" rows="5" placeholder="コメント" name="comment">{{ old('comment') }}</textarea>
+                                        <textarea class="w-100" cols="80" rows="5" placeholder="コメント" name="comment">{{ old('comment') }}</textarea>
                                     </td>
                                 </tr>
                                 <tr>
