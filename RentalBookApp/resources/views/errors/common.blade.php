609da2e389c9b9240bd71539704e52da492873bb
Jin <messialbest@gmail.com>
2020-07-23 20:14:15 +0900

    Update 共通エラー画面

--- RentalBookApp/resources/views/errors/common.blade.php
+++ RentalBookApp/resources/views/errors/common.blade.php
@@ -1,20 +1,9 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>エラー画面</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-        <!-- Styles -->
-        <link href="{{ asset('css/common.css') }}" rel="stylesheet">
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    </head>
-    <body> 
-        <div class="m-4">
-            <img class="w-100 error-image" src="{{ asset('storage/errorback.png') }}">
-        </div>
-    </body>
-</html>
+@section('content')
+<body> 
+    <div class="m-4">
+        <img class="w-100 error-image" src="{{ asset('storage/errorback.png') }}">
+    </div>
+</body>
+@endsection
