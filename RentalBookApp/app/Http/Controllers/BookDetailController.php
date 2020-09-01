5efa0302b25f9581c27c9fa05818376697df54d6
Shinya-Takiyoshi <garo.savior@gmail.com>
2020-09-01 19:46:46 +0900

    try catch transaction 実装

--- RentalBookApp/app/Http/Controllers/BookDetailController.php
+++ RentalBookApp/app/Http/Controllers/BookDetailController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use App\Book;
 use App\Comment;
 use App\Notice;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class BookDetailController extends Controller
 {
@@ -79,38 +81,49 @@ class BookDetailController extends Controller
         $user_id = $request->session()->get('user_id');
         $body = $request->input('comment');
 
-        //　コメントが初回であるか確認する。
-        $commentCnt = Comment::bookIdEqual($book_id)->count();
+        try {
+            // トランザクション
+            DB::transaction(function () use ($book_id, $user_id, $body) {
+                //　コメントが初回であるか確認する。
+                $commentCnt = Comment::bookIdEqual($book_id)->count();
 
-        $comment = new Comment;
-        $comment->user_id = $user_id;
-        $comment->book_id = $book_id;
-        $comment->body = $body;
-        $comment->save();
+                // コメント登録
+                $comment = new Comment;
+                $comment->user_id = $user_id;
+                $comment->book_id = $book_id;
+                $comment->body = $body;
+                $comment->save();
 
-        // 初回コメントの場合
-        if ($commentCnt == 0) {
-            // お知らせテーブル登録
-            $notice = new Notice;
-            $notice->user_id = $user_id;
-            $notice->book_id = $book_id;
-            $notice->body = $body;
-            $notice->save();
-        } else {
-            // 2回目以降
-            // お知らせテーブル登録
-            $comments = Comment::select('user_id')
-                ->distinct()->where('book_id', $book_id)->where('user_id', '<>', $user_id)->get();
+                // 初回コメントの場合
+                if ($commentCnt == 0) {
+                    // お知らせテーブル登録
+                    $notice = new Notice;
+                    $notice->user_id = $user_id;
+                    $notice->book_id = $book_id;
+                    $notice->body = $body;
+                    $notice->save();
+                } else {
+                    // 2回目以降
+                    // お知らせテーブル登録
+                    $comments = Comment::select('user_id')
+                        ->distinct()->where('book_id', $book_id)->where('user_id', '<>', $user_id)->get();
 
-            foreach ($comments as $target) {
-                $notice = new Notice;
-                $notice->user_id = $target->user_id;
-                $notice->book_id = $book_id;
-                $notice->body = $body;
-                $notice->save();
-            }
+                    foreach ($comments as $target) {
+                        $notice = new Notice;
+                        $notice->user_id = $target->user_id;
+                        $notice->book_id = $book_id;
+                        $notice->body = $body;
+                        $notice->save();
+                    }
+                }
+            });
+            return redirect()->route('bookdetail', ['book_id' => $book_id]);
+        } catch (\Exception $e) {
+            //ログ出力
+            Log::error($e->getMessage());
+            //エラー表示
+            abort(500, 'Internal error. Fail to update book data');
         }
-        return redirect()->route('bookdetail', ['book_id' => $book_id]);
     }
 
     /**
@@ -121,8 +134,18 @@ class BookDetailController extends Controller
     {
         // リクエストパラメータ
         $book_id = $request->input('book_id');
-        Comment::commentIdEqual($request->input('comment_id'))->delete();
-        return redirect()->route('bookdetail', ['book_id' => $book_id]);
+        $comment_id = $request->input('comment_id');
+        try {
+            DB::transaction(function () use ($comment_id) {
+                Comment::commentIdEqual($comment_id)->delete();
+            });
+            return redirect()->route('bookdetail', ['book_id' => $book_id]);
+        } catch (\Exception $e) {
+            //ログ出力
+            Log::error($e->getMessage());
+            //エラー表示
+            abort(500, 'Internal error. Fail to delete comment data');
+        }
     }
 
     /**
@@ -131,9 +154,18 @@ class BookDetailController extends Controller
      */
     public function updateNewFlg($book_id = '0', $notice_id = 0)
     {
-        $notices  = Notice::find($notice_id);
-        $notices->new_flag = 0;
-        $notices->save();
-        return redirect()->route('bookdetail', ['book_id' => $book_id]);
+        try {
+            DB::transaction(function () use ($notice_id) {
+                $notices  = Notice::find($notice_id);
+                $notices->new_flag = 0;
+                $notices->save();
+            });
+            return redirect()->route('bookdetail', ['book_id' => $book_id]);
+        } catch (\Exception $e) {
+            //ログ出力
+            Log::error($e->getMessage());
+            //エラー表示
+            abort(500, 'Internal error. Fail to uddate notice data');
+        }
     }
 }
