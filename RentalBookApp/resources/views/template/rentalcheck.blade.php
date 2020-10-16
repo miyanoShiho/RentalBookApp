a4d5b6e78be87159722344afcdd0971164d51796
JinKanekama <messialbest@gmail.com>
2020-10-17 01:27:45 +0900

    fix: レンタル画面のレイアウト修正

--- RentalBookApp/resources/views/template/rentalcheck.blade.php
+++ RentalBookApp/resources/views/template/rentalcheck.blade.php
@@ -7,15 +7,15 @@
 
 @section('content')
     <div class=row>
-        <div class="col-md-4">
+        <div class="col-md-3">
         </div>
-        <div class="col-md-4">
+        <div class="col-md-6">
             <div class="title h3">タイトル　：{{$title}}</div>
             <div class="username h3">ユーザー名：{{$offer_user_name}}</div>
             @yield('checkWords')
             @yield('underContent')
         </div>
-        <div class="col-md-4">
+        <div class="col-md-3">
         </div>
     </div>
 @endsection
